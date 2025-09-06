02832ff905909b37f410e61bf00f5d76d307da1e
JUND Guy <gjund@free.fr>
2025-09-06 09:43:49 +0200

    KYPHR 2.5

--- index.php
+++ index.php
@@ -1,5 +1,23 @@
 <?php
 
+$users = [
+    [
+        'full_name' => 'Mickaël Andrieu',
+        'email' => 'mickael.andrieu@exemple.com',
+        'age' => 34,
+    ],
+    [
+        'full_name' => 'Mathieu Nebra',
+        'email' => 'mathieu.nebra@exemple.com',
+        'age' => 34,
+    ],
+    [
+        'full_name' => 'Laurène Castor',
+        'email' => 'laurene.castor@exemple.com',
+        'age' => 28,
+    ],
+];
+
 $recipes = [
     [
         'title' => 'Cassoulet',
@@ -20,31 +38,62 @@ $recipes = [
         'is_enabled' => true,
     ],
 ];
+
+function displayAuthor(string $authorEmail, array $users): string
+{
+    foreach ($users as $user) {
+        if ($authorEmail === $user['email']) {
+            return $user['full_name'] . '(' . $user['age'] . ' ans)';
+        }
+    }
+}
+
+function isValidRecipe(array $recipe): bool
+{
+    if (array_key_exists('is_enabled', $recipe)) {
+        $isEnabled = $recipe['is_enabled'];
+    } else {
+        $isEnabled = false;
+    }
+
+    return $isEnabled;
+}
+
+function getRecipes(array $recipes): array
+{
+    $valid_recipes = [];
+
+    foreach ($recipes as $recipe) {
+        if (isValidRecipe($recipe)) {
+            $valid_recipes[] = $recipe;
+        }
+    }
+
+    return $valid_recipes;
+}
+
 ?>
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-    <title>Affichage des recettes</title>
+    <title>Recettes de cuisine</title>
     <link
-            href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
-            rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+        rel="stylesheet"
     >
 </head>
 <body>
 <div class="container">
-    <h1>Affichage des recettes</h1>
-    <!-- Boucle sur les recettes -->
-    <?php foreach ($recipes as $recipe) : ?>
-        <!-- si la clé existe et a pour valeur "vrai", on affiche -->
-        <?php if (array_key_exists('is_enabled', $recipe) && $recipe['is_enabled'] == true) : ?>
-            <article>
-                <h3><?php echo $recipe['title']; ?></h3>
-                <div><?php echo $recipe['recipe']; ?></div>
-                <i><?php echo $recipe['author']; ?></i>
-            </article>
+    <h1>Liste des recettes de cuisine</h1>
 
-        <?php endif; ?>
+    <?php foreach (getRecipes($recipes) as $recipe) : ?>
+        <article>
+            <h3><?php echo $recipe['title']; ?></h3>
+            <div><?php echo $recipe['recipe']; ?></div>
+            <i><?php echo displayAuthor($recipe['author'], $users); ?></i>
+        </article>
     <?php endforeach ?>
 </div>
 </body>
 </html>
+
