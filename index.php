b5d420ed6479366926623bc5b2af8486fcf51a5f
JUND Guy <gjund@free.fr>
2025-09-08 09:20:43 +0200

    KYPHR 3.1

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-<!-- inclusion des variables et fonctions -->
 <?php
 require_once(__DIR__ . '/variables.php');
 require_once(__DIR__ . '/functions.php');
@@ -12,14 +11,12 @@ require_once(__DIR__ . '/functions.php');
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Site de recettes - Page d'accueil</title>
     <link
-        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
-        rel="stylesheet"
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+            rel="stylesheet"
     >
 </head>
 <body class="d-flex flex-column min-vh-100">
     <div class="container">
-
-        <!-- inclusion de l'entête du site -->
         <?php require_once(__DIR__ . '/header.php'); ?>
         <h1>Site de recettes</h1>
 
@@ -31,8 +28,6 @@ require_once(__DIR__ . '/functions.php');
             </article>
         <?php endforeach ?>
     </div>
-
-    <!-- inclusion du bas de page du site -->
     <?php require_once(__DIR__ . '/footer.php'); ?>
 </body>
 </html>
